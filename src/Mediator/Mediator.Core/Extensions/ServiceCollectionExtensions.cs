72747a5f4c4f2ce46af126b8074db5a2ecf89c77
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-04-15 09:59:41 -0300

    Adiciona suporte a DI para Mediator
    - Adicionado pacote `Microsoft.Extensions.DependencyInjection` v6.0.0 ao `Mediator.Core.csproj`.
    - Criado `ServiceCollectionExtensions.cs` com extensão `AddMediator` para `IServiceCollection`.
    - Implementada função `ResolveAssemblies` para resolver assemblies.
    - Implementada função `RegisterHandlers` para registrar handlers no contêiner de serviços.

---
+++ src/Mediator/Mediator.Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,69 @@
+﻿using Mediator.Core.Interface;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Mediator.Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMediator(this IServiceCollection services, params object[] args)
+        {
+            var assemblies = ResolveAssemblies(args);
+
+            services.AddSingleton<IMediator, Mediator.Core.Implementation.Mediator>();
+
+            RegisterHandlers(services, assemblies, typeof(IRequestHandler<,>));
+            RegisterHandlers(services, assemblies, typeof(INotificationHandler<>));
+
+            return services;
+        }
+
+        private static Assembly[] ResolveAssemblies(object[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                return AppDomain.CurrentDomain.GetAssemblies()
+                    .Where(a => !a.IsDynamic && !string.IsNullOrEmpty(a.FullName))
+                    .ToArray();
+            }
+
+            if (args.All(a => a is Assembly))
+            {
+                return args.Cast<Assembly>().ToArray();
+            }
+
+            if (args.All(a => a is string))
+            {
+                var prefix = args.Cast<string>().ToArray();
+                return AppDomain.CurrentDomain
+                    .GetAssemblies()
+                    .Where(a => !a.IsDynamic &&
+                        !string.IsNullOrEmpty(a.FullName) &&
+                        prefix.Any(p => a.FullName!.StartsWith(p)))
+                    .ToArray();
+            }
+
+            throw new ArgumentException("Invalid arguments. Expected Assembly or string array.");
+        }
+
+        private static void RegisterHandlers(IServiceCollection services, Assembly[] assemblies, Type handlerInterface)
+        {
+            var types = assemblies.SelectMany(a => a.GetTypes())
+                .Where(t => t.IsClass && !t.IsAbstract)
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var interfaces = type.GetInterfaces()
+                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == handlerInterface);
+
+                foreach (var iface in interfaces)
+                {
+                    services.AddTransient(iface, type);
+                }
+            }
+        }
+    }
+}
